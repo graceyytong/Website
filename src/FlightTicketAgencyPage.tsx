e709cad4cc2698997067d655ece786becf2aff39
Daki <graceyytong@gmail.com>
2026-04-15 20:54:28 -0400

    Added ICP备案号

--- src/FlightTicketAgencyPage.tsx
+++ src/FlightTicketAgencyPage.tsx
@@ -224,6 +224,16 @@ const FlightTicketAgencyPage: React.FC = () => {
           </div>
         </div>
       </section>
+
+      <footer className="site-footer">
+        <div className="container footer-inner">
+          <span>沪ICP备2026010355号</span>
+          <span>|</span>
+          <span>
+            Copyright © {new Date().getFullYear()} 上海远晶旅游咨询有限公司 版权所有
+          </span>
+        </div>
+      </footer>
     </div>
   );
 };
